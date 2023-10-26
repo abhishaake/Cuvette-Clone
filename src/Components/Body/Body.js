df47b694d7c03e3d0ceeb61314c973d48892682c
Abhishek Verma <abhishekverma0203@gmail.com>
2023-10-26 14:03:45 +0530

    body content added + styling

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -10,17 +10,22 @@ import {
     ReferenceLine,
     ResponsiveContainer,
   } from 'recharts';
-
+import htmlIcon from "./Assets/html-icon.png";
+import trophyIcon from "./Assets/trophy-icon.png";
+import checkIcon from "./Assets/check-icon.png";
+import npIcon from "./Assets/np-icon.png";
 import "./Body.css";
 import graphData from "./graphData"
 
 function Body(){
 
     const data = graphData();
+
+
     const CustomizedDot = (props) => {
         const { cx, cy, stroke, payload, value } = props;
       
-        console.log(cx,cy,stroke,payload,value);
+        // console.log(cx,cy,stroke,payload,value);
         let xvalue = parseInt(payload.name.slice(0,-1));
 
         if(xvalue===37){
@@ -46,33 +51,98 @@ function Body(){
       };
 
     return (<>
-        <div style={{width:'100%',minHeight:'500px'}}>
+        <div className='body-container'>
 
-            <div style={{height:'200px',background:'aliceblue'}}></div>
-            <div className="body-graph">
-                <ResponsiveContainer width="100%" >
-                    <LineChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 20,
-                        right: 50,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                    >
-                        <XAxis dataKey="name" interval={19}/>
-                        <Tooltip position="top"/>
-                        <ReferenceLine x="37%" stroke="aliceblue" strokeWidth={100} />
+            <div className='body-div'>
+                <div className='body-div_div'>
+                    <div className='body-logo_div'>
+                        <img style={{width:'40px'}} src={htmlIcon}></img>
+                        <div style={{}}>
+                            <strong>Hypertext Markup Language</strong>
+                            <p style={{fontSize:'13px',color:'gray',margin:'4px 0'}}>
+                                Questions: 08 | Duration: 15 mins | Submitted on 5 June 2021
 
-                        <ReferenceLine x="37%" strokeDasharray="5 5" stroke="dodgerblue" position="start" label="37% Percentile" />
-                        <ReferenceLine x="72%" strokeDasharray="5 5" stroke="red" position="start" label="72% Percentile" />
-                        <Line dot={<CustomizedDot />} type="monotone" dataKey="value" stroke="lightgray" strokeWidth={2}/>
-                        <Line dot={false} type="monotone" dataKey="value2" stroke="white" strokeWidth={1}/>
-                    </LineChart>
-                </ResponsiveContainer>
+                            </p>
+                        </div>
+
+                    </div>
+                   
+                    <div>
+                        <button className="body-submit_button">Update</button>
+                    </div>
+                </div>
             </div>
+            <div className='body-div'>
+                <div className='body-header' style={{width:'100%',flexDirection:'column',alignItems:'start'}}>
+                    <div>
+                        <div><strong>Quick Statistics</strong></div>
+                    </div>
+                    <div className="body-stats_div">
+                        <div >
+                            <div className='body-icons'><img style={{width:'45%',margin:'auto'}} src={trophyIcon}></img></div>
+                            <div style={{margin:'3px 0 3px 20px'}}>
+                                <p style={{fontWeight:"1000",fontSize:'20px',margin:'2px 2px 4px'}}><strong>12,890</strong></p>
+                                <p style={{color:'gray',fontSize:'12px'}}>YOUR RANK</p>    
+                            </div>
+                            
+                        </div>
+                        <div >
+                            <div className='body-icons'><img style={{width:'55%',margin:'auto'}} src={npIcon}></img></div>
+                            <div style={{margin:'3px 0 3px 20px'}}>
+                                <p style={{fontWeight:"1000",fontSize:'20px',margin:'2px 2px 4px'}}><strong>37%</strong></p>
+                                <p style={{color:'gray',fontSize:'12px'}}>PERCENTILE</p>    
+                            </div>
+                        </div>
+                        <div style={{border:0,width:'90%'}}>
+                            <div className='body-icons'><img style={{width:'38%',borderRadius:'4px',margin:'auto'}} src={checkIcon}></img></div>
+                            <div style={{margin:'3px 0 3px 20px'}}>
+                                <p style={{fontWeight:"1000",fontSize:'20px',margin:'2px 2px 4px'}}><strong>07 / 15</strong></p>
+                                <p style={{color:'gray',fontSize:'12px'}}>CORRECT ANSWERS</p>    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='body-div'>
+                <div className='body-header'>
+                    <div style={{width:'70%'}}>
+                        <div><strong>Comparison Graph</strong></div>
+                        <div style={{margin:'15px 0 0'}}>You scored 37% Percentile which is lower than the average Percentile 72% of all engineers who took the assessment</div>
+                    </div>
+                    <div style={{margin:'auto 5% auto auto'}}>
+                    <div className='body-icons'><svg style={{margin:'auto',backgroundColor:'rgb(210 219 224)'}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-graph-up" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0Zm14.817 3.113a.5.5 0 0 1 .07.704l-4.5 5.5a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61 4.15-5.073a.5.5 0 0 1 .704-.07Z"/> </svg></div>
+                    
+                    </div>
+                </div>
+                <div style={{width:'100%'}}>
+                    <div className="body-graph">
+                        <ResponsiveContainer width="100%" >
+                            <LineChart
+                            width={500}
+                            height={300}
+                            data={data}
+                            margin={{
+                                top: 20,
+                                right: 50,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                            >
+                                <XAxis dataKey="name" interval={19}/>
+                                <Tooltip position="top"/>
+                                <ReferenceLine x="37%" stroke="aliceblue" strokeWidth={100} />
+
+                                <ReferenceLine x="37%" strokeDasharray="5 5" stroke="dodgerblue" position="start" label="37% Percentile" />
+                                <ReferenceLine x="72%" strokeDasharray="5 5" stroke="red" position="start" label="72% Percentile" />
+                                <Line dot={<CustomizedDot />} type="monotone" dataKey="value" stroke="lightgray" strokeWidth={2}/>
+                                <Line dot={false} type="monotone" dataKey="value2" stroke="white" strokeWidth={1}/>
+                            </LineChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
+                
+            </div>
+            
 
         </div>
 
--- src/Components/Body/graphData.js
+++ src/Components/Body/graphData.js
@@ -1,3 +1,20 @@
+function nthroot(x, n)
+   {
+    try{
+      var negate = n % 2 == 1 && x < 0;
+      if(negate)
+        x = -x;
+      var possible = Math.pow(x, 1 / n);
+      n = Math.pow(possible, n);
+      if(Math.abs(x - n) < 1 && (x > 0 == n > 0))
+        return negate ? -possible : possible;
+    }
+    catch(e){console.log("error ... " + e);}
+    
+   }
+
+
+
 function graphData(){
   const data = [];
   const fixed = [10,40,100,220,100,140,110,125,40,50,10];
@@ -16,15 +33,19 @@ function graphData(){
     else{
       let band = Math.floor(i/10);
       if(band<10){
-        let dif = parseInt(fixed[band+1]) - parseInt(fixed[band]);
-        let val = Math.round(dif/intervals);
-        let cur = fixed[band] + val;
+        // let dif = parseInt(fixed[band+1]) - parseInt(fixed[band]);
+        // let val = Math.round(dif/intervals);
+        // let cur = fixed[band] + val;
+        let div =  (parseInt(fixed[band+1])/parseInt(fixed[band]));
+        let val = nthroot(div,9);
+        // console.log("nth root : " + val);
+        let cur = fixed[band]*val;
         for(let j=0;j<intervals;j++){
           obj = {
             name: i+j+'%',
-            value: cur,
+            value: Math.round(cur),
           }
-          cur += val;
+          cur = fixed[band]*(Math.pow(val,j+2));
           data.push(obj);
         }
       }
@@ -32,7 +53,7 @@ function graphData(){
     }
     
   }
-
+  // console.log(data);
   return data;
 
 }
